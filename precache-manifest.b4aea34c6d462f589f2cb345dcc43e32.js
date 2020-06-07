71e641ca2e943175bde6d408d6ba85d34b625f97
Ayush Jain <ayushjain8719@gmail.com>
2020-06-07 23:48:18 +0530

    Updates

---
+++ precache-manifest.b4aea34c6d462f589f2cb345dcc43e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255ac8d50648b27778b650ff37d07888",
+    "url": "/quicklyric/index.html"
+  },
+  {
+    "revision": "dd6f9781c22d3f9febfb",
+    "url": "/quicklyric/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5ef6592e79eed0438749",
+    "url": "/quicklyric/static/js/2.62079622.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/quicklyric/static/js/2.62079622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6f9781c22d3f9febfb",
+    "url": "/quicklyric/static/js/main.414f1769.chunk.js"
+  },
+  {
+    "revision": "d871bc4600c6e7e0f8fd",
+    "url": "/quicklyric/static/js/runtime-main.1bfea77a.js"
+  }
+]);
